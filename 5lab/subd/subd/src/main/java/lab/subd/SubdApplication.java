741a45f38f0f4c9f384c36b51e17e860edcafe96
taurielke <taurielkee@yandex.ru>
2022-04-06 12:10:13 +0400

    initial commit or smth

---
+++ 5lab/subd/subd/src/main/java/lab/subd/SubdApplication.java
@@ -0,0 +1,13 @@
+package lab.subd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubdApplication.class, args);
+	}
+
+}
---
+++ 5lab/subd/subd/src/test/java/lab/subd/SubdApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.subd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
